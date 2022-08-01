c61062f75ff27f3926758c5d78d1f4a75b8854e0
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-08-02 00:10:12 +0600

    if condition

---
+++ if_condition.py
@@ -0,0 +1,16 @@
+from tokenize import Double
+
+
+name=input("Name:  ")
+if name=="shaon":
+    print(f"Hi {name} .I am Github")
+elif name=="john":
+    print(f"You are not the owner")
+else:
+    print("Invalid name")
+
+number =int(input("Enter any Number"))
+if number > 0:
+    print("Its an positive number")
+else:
+    print("its not a valid")
