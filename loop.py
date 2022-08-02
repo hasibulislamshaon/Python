4fdc096e59dacfbf6bbb55bb0f4d037c6fdebde2
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-08-03 00:16:08 +0600

    new loop beganing

---
+++ loop.py
@@ -0,0 +1,7 @@
+number=int(input("Enter starting number"))
+name=int(input("Enter some names as string: "))
+
+print("All Number")
+for number in range(name):
+    number=number+1
+    print(number)
