6f4eb302f786861906ac0b4297cd6702c8913192
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-08-02 17:27:03 +0600

    List and set practice

---
+++ set,list.py
@@ -0,0 +1,25 @@
+#name of city in bangladesh
+bangladesh_city_name = ["Dhaka","Rajsahi","Sylet","Chittagong","Rangpur"]
+print(bangladesh_city_name[0])
+
+#append method to add some thing to the list at the end of the list 
+bangladesh_city_name.append("Khulna")
+print("Unsorted= ",bangladesh_city_name)
+
+#sort method in list
+bangladesh_city_name.sort()
+print("Sorted= ",bangladesh_city_name)
+
+
+#created a set
+name=set()
+
+#adding in the set
+name.add(1)
+name.add(2)
+name.add("a")
+name.add("a")
+name.add(2)
+name.add(4)
+
+print(name)
