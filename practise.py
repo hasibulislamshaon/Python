84dd387dcdb049c43c16b6545248af81f33c157e
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-08-07 12:49:26 +0600

    def function

---
+++ practise.py
@@ -0,0 +1,21 @@
+def EXPECTED_BAKE_TIME():
+   return int(40)
+   """This function will tell the expected bake time for the recipe"""
+
+def bake_time_remaining(elapsed_bake_time):
+    """This function will return the remaining bake time"""
+    return 40-elapsed_bake_time
+
+def preparation_time_in_minutes(number_of_layers):
+    """This function will tell the preparation time"""
+    return number_of_layers*2
+
+def elapsed_time_in_minute(number_of_layers,elapsed_bake_time):
+
+    time=int(number_of_layers*2)
+    return elapsed_bake_time+time
+"""
+Return elapsed cooking time.
+This function takes two numbers representing the number of layers & the time already spent 
+baking and calculates the total elapsed minutes spent cooking the lasagna.
+"""
